3fbecaa95ab68d801dee567efcd2a6a6d3e29611
Nic <nicolyakira@gmail.com>
2023-07-23 21:36:14 -0300

    Update joined the server...

---
+++ 004-Trilha Java/Angular/fundamentos-angular/src/app/primeiro-componente/primeiro-componente-component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-primeiro-componente",
+    template: "<h2>Ppppppppppppppppppppppppppppppppppppppppppppppppppppppprimeiro componente"
+})
+
+export class PrimeiroComponenteComponent {}
